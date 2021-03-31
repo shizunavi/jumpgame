80a96c3d921d8071126f1cfdab173d7ac505aa60
shizunavi <80692445+shizunavi@users.noreply.github.com>
2021-03-31 21:14:08 +0900

    needed files uploaded

---
+++ script.js
@@ -0,0 +1,26 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block"); //character, blockのdivを読み込む
+var counter=0; //スコア用
+
+
+function jump(){
+    if(character.classList == "animate"){return}; //すでにジャンプしている時は何もしない
+    character.classList.add("animate"); //CSSでanimateプロパティを追加する
+    setTimeout(function(){
+        character.classList.remove("animate"); //動作が終わったらanimateを取り除き、クリックすればまた動くようにする
+        },300); //300ミリ秒ごとに動作終わったかどうかの判定
+    };
+
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top")); //characterの上辺定義
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left")); //blockの左辺定義
+    if(blockLeft<15 && blockLeft>-20 && characterTop>=140){　//blockLeft<20 && blockLeft>-20なのでblockはcharacterの真下にある。characterTop>=130であればblockと２０px以上重なっている
+        block.style.animation = "none"; //ぶつかったら一旦ゲームが止まるようにする
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear"; //スコア表示させたらまた動かす
+    }else{
+        counter++; //うまく飛べたのでスコア増える
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);　//下のスコア表記を逐一書き換える。なぜcounter/100が必要？
+    }
+}, 10); //10ミリ秒ごとにチェックを行う
